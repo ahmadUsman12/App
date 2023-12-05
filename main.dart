2e05709b225a393bfd7f57ccb3858260b3775ee7
ahmadUsman12 <152734397+ahmadUsman12@users.noreply.github.com>
2023-12-05 16:35:39 +0500

    Add files via upload

--- main.dart
+++ main.dart
@@ -5,104 +5,107 @@ import 'package:intern_app/SignUp_Screen.dart';
 void main() {
   runApp(const MyApp());
 }
+
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-          home: Scaffold(
-            backgroundColor: Colors.white,
-            body: SafeArea(
-                child:Column(
-                  children:[
-                    SizedBox(height: 20,),
-                    Align(
-                      alignment: Alignment.center,
-                      child: Text(
-                        'a.g.bell',
-                        style: TextStyle(
-                          fontSize : 50,
-                          fontWeight: FontWeight.w400,
-                          color: Colors.black,
-                        ),
-                      ),
-                    ),
-                    SizedBox(
-                      height: 20,
-                    ),
-                    SizedBox(
-                      width: double.infinity,
-                      height: 400,
-                      child: Image(image: AssetImage(
-                        'assets/illus.jpeg',
-                      ),
-                      ),
-                    ),
-
-                     SizedBox(
-                       height: 49,
-                       width: 370,
-                       child: TextButton(
-                          onPressed:(){
-                            Navigator.push(context, MaterialPageRoute(builder: (context)=> const SignInScreen()));
-                          },
-                          child: Text(
-                              'SignIn',
-                            style: TextStyle(
-                              color: Colors.blue,
-                              fontSize: 20,
-                              fontWeight: FontWeight.w500,
-                            ),
-
-                          ),
-                          style: ButtonStyle(
-                            foregroundColor: MaterialStateProperty.all<Color>(Colors.blue),
-                              shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                                  RoundedRectangleBorder(
-                                      borderRadius: BorderRadius.circular(20),
-                                      side: BorderSide(color: Colors.blue)
-                                  )
-                              )
-                          )
-                        ),
-                     ),
-                    SizedBox(height: 20,),
-                    SizedBox(
-                      height: 49,
-                      width: 370,
-                      child: TextButton(
-                          onPressed:(){
-                            Navigator.of(context).push(MaterialPageRoute(builder: (context)=> const SignUpScreen()));
-                          },
-                          child: Text(
-                            'SignUp',
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 20,
-                              fontWeight: FontWeight.w500,
-                            ),
-
-                          ),
-                          style: ButtonStyle(
-                              backgroundColor: MaterialStateProperty.all<Color>(Colors.blue),
-                              shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                                  RoundedRectangleBorder(
-                                      borderRadius: BorderRadius.circular(20),
-                                      side: BorderSide(color: Colors.blue)
-                                  )
-                              )
-                          )
-                      ),
-                    ),
-
-                  ],
-                ),
-            ),
-            ),
+      home: MyHomePage(),
     );
-
   }
 }
 
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Column(
+          children: [
+            SizedBox(height: 20),
+            Align(
+              alignment: Alignment.center,
+              child: Text(
+                'a.g.bell',
+                style: TextStyle(
+                  fontSize: 50,
+                  fontWeight: FontWeight.w400,
+                  color: Colors.black,
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            SizedBox(
+              width: double.infinity,
+              height: 400,
+              child: Image.asset('assets/illus.jpeg'),
+            ),
+            SizedBox(
+              height: 49,
+              width: 370,
+              child: TextButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const SignInScreen()),
+                  );
+                },
+                child: Text(
+                  'SignIn',
+                  style: TextStyle(
+                    color: Colors.blue,
+                    fontSize: 20,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+                style: ButtonStyle(
+                  foregroundColor: MaterialStateProperty.all<Color>(Colors.blue),
+                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20),
+                      side: BorderSide(color: Colors.blue),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            SizedBox(
+              height: 49,
+              width: 370,
+              child: TextButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const SignUpScreen()),
+                  );
+                },
+                child: Text(
+                  'SignUp',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all<Color>(Colors.blue),
+                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20),
+                      side: BorderSide(color: Colors.blue),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
